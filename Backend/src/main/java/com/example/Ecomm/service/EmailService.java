58f3d764fb2de990c8c021c3791e5d2c29d1a0db
Nikhil Kumar <kumarnikhil0683@gmail.com>
2025-12-12 15:56:59 +0530

    fixing mail problem 2

--- Backend/src/main/java/com/example/Ecomm/service/EmailService.java
+++ Backend/src/main/java/com/example/Ecomm/service/EmailService.java
@@ -6,12 +6,16 @@ import com.example.Ecomm.model.User;
 import com.example.Ecomm.repo.OrderRepository;
 import com.example.Ecomm.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+
+    @Value("${EMAIL_FROM_SENDGRID}")
+    private String senderEmail;
 
     @Autowired
     private JavaMailSender mailSender;
@@ -36,7 +40,9 @@ public class EmailService {
 
         User user = order.getUser();
 
+
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(senderEmail);
         message.setTo(user.getEmail());
         message.setSubject("✅ Payment Successful - ShopEase");
         message.setText("Hi " + user.getName() + ",\n" +
